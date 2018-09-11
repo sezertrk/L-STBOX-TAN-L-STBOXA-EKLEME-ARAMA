dfa92729fade6a51f94c9488f61685e6b57da1ae
sezertrk <43032011+sezertrk@users.noreply.github.com>
2018-09-11 14:37:38 +0300

    Add files via upload

---
+++ LİSTBOX TAN LİSTBOXA EKLEME ARAMA/LİSTBOX TAN LİSTBOXA EKLEME ARAMA/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LİSTBOX_TAN_LİSTBOXA_EKLEME_ARAMA
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int deger = -1;
+            for (int i = 0; i < listBox1.Items.Count; i++) 
+            {
+                if (listBox1.Items[i].ToString() == textBox1.Text)
+                {
+                    deger = i;
+                }
+                if (deger == -1)
+                {
+                    MessageBox.Show("Girilen Değer Bulunamadı");
+                }
+                else
+                {
+                    listBox2.Items.Add(listBox1.Items[deger].ToString());
+                    listBox1.Items.RemoveAt(deger);
+                }
+            }
+        }
+    }
+}
+
+        
+    
+    
+
+
+
